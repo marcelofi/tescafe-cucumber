f59ba0a484ec5d95033b94ac54587271cd83f077
ma.fernandez <ma.fernandez@ptytechnologies.com>
2022-06-22 15:51:12 -0400

    update the code

--- page-object/components/layout-top-nav.ts
+++ page-object/components/layout-top-nav.ts
@@ -1,31 +1,55 @@
-import { Selector,t } from "testcafe";
+import { Selector, t } from 'testcafe';
 
-class LayoutTopNav{        
-        sportsIcon:Selector=Selector('a[routerlink="/sportsbook"]');
-        livebettingIcon:Selector=Selector('a[routerlink="/live-betting"]');
-        casinoIcon:Selector=Selector('a[routerlink="/casino"]');
-        raceBookIcon:Selector=Selector('a[routerlink="/horses"]');
-        pokerIcon:Selector=Selector('a[routerlink="/poker"]');
-        esportIcon:Selector=Selector('a[routerlink="/esports"]');
-        contestIcon:Selector=Selector('a[routerlink="/contests"]');
-        promosIcon:Selector=Selector('a[routerlink="/promotions"]');
-        logobetonline:Selector=Selector('img[src="/assets/images/logo.svg"]');        
-        aboutUsFooter:Selector=Selector('#btnAboutUs');
-        activeIcon:Selector=Selector('.categories > .active');
-    
+class LayoutTopNav {
+  elements = {
+    sportsIcon: Selector(
+      `body > app-root:nth-child(1) > app-header:nth-child(1) > nav:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1)`
+    ),
+    livebettingIcon: Selector(
+      `body > app-root:nth-child(1) > app-header:nth-child(1) > nav:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(4) > a:nth-child(1)`
+    ),
+    casinoIcon: Selector(
+      `body > app-root:nth-child(1) > app-header:nth-child(1) > nav:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(6) > a:nth-child(1)`
+    ),
+    raceBookIcon: Selector(
+      `body > app-root:nth-child(1) > app-header:nth-child(1) > nav:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(7) > a:nth-child(1)`
+    ),
+    pokerIcon: Selector(
+      `body > app-root:nth-child(1) > app-header:nth-child(1) > nav:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(8) > a:nth-child(1)`
+    ),
+    esportIcon: Selector(
+      `body > app-root:nth-child(1) > app-header:nth-child(1) > nav:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(9) > a:nth-child(1)`
+    ),
+    contestIcon: Selector(
+      `body > app-root:nth-child(1) > app-header:nth-child(1) > nav:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(10) > a:nth-child(1)`
+    ),
+    promosIcon: Selector(
+      `body > app-root:nth-child(1) > app-header:nth-child(1) > nav:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(11) > a:nth-child(1)`
+    ),
+    logobetonline: Selector(`.brand`),
+    aboutUsFooter: Selector('#btnAboutUs'),
+    activeIcon: Selector('.categories > .active'),
+  };
 
-    async headerBarDisplayed(){
-        await t
-        .expect(this.sportsIcon.exists).ok()
-        .expect(this.livebettingIcon.exists).ok()
-        .expect(this.casinoIcon.exists).ok()
-        .expect(this.raceBookIcon.exists).ok()
-        .expect(this.pokerIcon.exists).ok()
-        .expect(this.esportIcon.exists).ok()
-        .expect(this.contestIcon.exists).ok()
-        .expect(this.promosIcon.exists).ok()
-    }
-    
+  async headerBarDisplayed() {
+    await t
+      .expect(this.elements.sportsIcon.exists)
+      .ok()
+      .expect(this.elements.livebettingIcon.exists)
+      .ok()
+      .expect(this.elements.casinoIcon.exists)
+      .ok()
+      .expect(this.elements.raceBookIcon.exists)
+      .ok()
+      .expect(this.elements.pokerIcon.exists)
+      .ok()
+      .expect(this.elements.esportIcon.exists)
+      .ok()
+      .expect(this.elements.contestIcon.exists)
+      .ok()
+      .expect(this.elements.promosIcon.exists)
+      .ok();
+  }
 }
 
 export default new LayoutTopNav();
